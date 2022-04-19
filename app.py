96f5d2d85e9463656e71f9b058e1da87bff0bea2
BlinkyExec <blinkyexecutive@gmail.com>
2022-04-19 14:14:02 +0300

    make limit orders

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
+def order(side, quantity, symbol, price, order_type=ORDER_TYPE_LIMIT):
     try:
         print(f"sending order {order_type} - {side} {quantity} {symbol}")
 
@@ -16,7 +16,7 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
 
         #client.futures_change_margin_type(symbol=symbol, marginType='CROSSED')
         #client.futures_change_leverage(symbol=symbol, leverage=10)
-        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        order = client.futures_create_order(symbol=symbol, side=side, price=price, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
@@ -41,7 +41,8 @@ def webhook():
     side = data['strategy']['order_action'].upper()
     quantity = data['strategy']['order_contracts']
     symbol = data['ticker']
-    order_response = order(side, quantity, symbol)
+    price = data['strategy']['order_price']
+    order_response = order(side, quantity, symbol, price)
 
     if order_response:
         return {
