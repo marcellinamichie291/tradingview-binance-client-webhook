75056c8ee3ed9d764b358849a02adc7aa309ade3
BlinkyExec <blinkyexecutive@gmail.com>
2022-04-19 11:50:25 +0300

    remove problem parameters

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
 
         #client.futures_change_margin_type(symbol=symbol, marginType='CROSSED')
         #client.futures_change_leverage(symbol=symbol, leverage=10)
-        order = client.futures_create_order(symbol=symbol, side=side, timeInForce='GTC', type=order_type, quantity=quantity)
+        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
