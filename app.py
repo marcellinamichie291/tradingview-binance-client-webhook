ace13031a7c0e6ddc541db60029c3a79f5f652d1
BlinkyExec <blinkyexecutive@gmail.com>
2022-04-19 11:24:42 +0300

    testing order

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
         #example order (not futures)
         """ order = client.create_order(symbol=symbol, side=side, type=order_type, quantity=quantity) """
 
-        client.futures_change_margin_type(symbol=symbol, marginType='CROSSED')
+        #client.futures_change_margin_type(symbol=symbol, marginType='CROSSED')
         client.futures_change_leverage(symbol=symbol, leverage=10)
-        order = client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
+        order = client.futures_create_order(symbol=symbol, side=side, timeInForce='GTC', type=order_type, quantity=quantity)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
