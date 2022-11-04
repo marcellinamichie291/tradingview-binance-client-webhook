de8d9217540656a16e73b994bf8493b6a0cd15f5
BlinkyExec <88986326+BlinkyExec@users.noreply.github.com>
2022-11-04 13:58:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-WEBHOOK_PASSPHRASE = "dYU8Tv#U&6$Fhtz"
+WEBHOOK_PASSPHRASE = "YoO$h5224uMU"
 
-API_KEY = 'JcjlRm6G8ST53ZspwApgK7i9NlnOPqJ1ssEyQNvlXdUBOpQzZa0NV0aMUaGDzWwB'
-API_SECRET = 'ujuLcJwCzQqxHFzsoU7UoNcZJEHVODDi06EEVXTVC4toTdlE0hzejAAzVVUv4kKO'
+API_KEY = 'Ic7SCRzTleGon2DjsqrBf31AT2I9Ly3QI81zivemgiKfXxFNh6NgRe662nU4zGkG'
+API_SECRET = '5aM7hP5jHaS5Op0mMV5Vd8dcQpwT4CdpOHyihVHF8HTLAuNU8ORuMMllcXwpOQ4L'
 
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/992173551295680693/y9urnD6Z4BgvOfNOxaAynh7F-yp8N-V2n_7vCG5qDKdKPAw3wuSEvHS2X00Rbco5JVwP" # use a string containing your discord webhook url to enable
-DISCORDPRO_WEBHOOK_URL = "https://discord.com/api/webhooks/1017898659666468904/IYM7liHA0LI-sPEw_eH8x0gOBvSAauAfcqkNaEqZ1uXJVdeB-JxLtS2UqCXnTyayQefH"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1038010531421888562/MaxfxBfmKXkeX4Oo4Mn8PHRCTEOZOsztIiHmktuREIuZmd8AWNsEEikxsAbdtjxN2D02" # use a string containing your discord webhook url to enable
+DISCORDPRO_WEBHOOK_URL = "https://discord.com/api/webhooks/1038010531421888562/MaxfxBfmKXkeX4Oo4Mn8PHRCTEOZOsztIiHmktuREIuZmd8AWNsEEikxsAbdtjxN2D02"
